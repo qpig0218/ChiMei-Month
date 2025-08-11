abe5d8c9df6a6a31bba5e5af9cc7c68c2dfa62a6
qpig0218 <131685585+qpig0218@users.noreply.github.com>
2025-08-11 11:19:37 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // GitHub Pages 子路徑：qpig0218/ChiMei-Month
 export default defineConfig({
-  base: '/ChiMei-Month/',
   plugins: [react()],
+  base: '/ChiMei-Month/',
 })
